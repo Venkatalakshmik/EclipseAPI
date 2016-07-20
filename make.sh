6a5b4d9204ed6d3e5a3ee841141a7dd91a17e11c
Venkatalakshmik <vkondapalli@miraclesoft.com>
2016-07-21 01:51:16 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 
 #!/bin/sh
 
-apigeetool deploynodeapp -u 'kondapalli.venkatalakshmi@gmail.com' -p 'Welcome@2998' -o venkatalakshmi -e test -n 'mock123' -d . -m 1.js -b /mock123 -R --verbose --debug
+apigeetool deploynodeapp -u 'kondapalli.venkatalakshmi@gmail.com' -p 'Welcome@2998' -o venkatalakshmi -e test -n 'mock123' -d . -m 1.html -b /mock123 -R --verbose --debug
 echo "You can now test out the API proxy by entering http://$org-$env.apigee.net/mock123 in cURL or your browser"
