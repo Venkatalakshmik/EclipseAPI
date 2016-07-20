6525fb01a008be9966098f0978791b517c95a6fd
Venkatalakshmik <vkondapalli@miraclesoft.com>
2016-07-21 01:32:15 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 
 #!/bin/sh
 
-apigeetool deploynodeapp -u 'kondapalli.venkatalakshmi@gmail.com' -p 'Welcome@2998' -o venkatalakshmi -e test -n 'diego' -d . -m app.js -b /mock -R --verbose --debug
+apigeetool deploynodeapp -u 'kondapalli.venkatalakshmi@gmail.com' -p 'Welcome@2998' -o venkatalakshmi -e test -n 'diego' -d . -m NewFile.jsp -b /mock -R --verbose --debug
 echo "You can now test out the API proxy by entering http://$org-$env.apigee.net/diego-nock-mock in cURL or your browser"
